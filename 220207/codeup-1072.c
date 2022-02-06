ec94a22a326fc8302a363d4297a9a19fcabb682b
Yehyung Kim <98684781+yehyung0117@users.noreply.github.com>
2022-02-07 03:19:39 +0900

    Create codeup-1072.c

---
+++ 220207/codeup-1072.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+    int main()
+    
+{
+    int n, m;
+    
+    scanf("%d", &n);
+    n--;
+    reget:
+    
+    scanf("%d", &m);
+    printf("%d\n", m);
+    
+    if(n-- != 0) goto reget;
+
+    return 0;
+}
