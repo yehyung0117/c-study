347e16ab6f940b683b8f690785459d68e87903a6
Yehyung Kim <98684781+yehyung0117@users.noreply.github.com>
2022-02-07 03:07:09 +0900

    Create codeup-1071.c

---
+++ 220207/codeup-1071.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+    int main()
+    
+{
+    int n;
+        
+    reload:
+    scanf("%d", &n);
+        
+    if(n==0) goto end;
+        
+    printf("%d\n", n);
+    
+    if(n!=0) goto reload;
+    end:
+        
+    return 0;
+}
